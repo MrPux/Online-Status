407f924d8faa219bd0224227139ff6464cc87cd0
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-22 15:01:57 -0500

    First commit with instructions

---
+++ Online_Status.py
@@ -0,0 +1,5 @@
+#   Write a function named 'online_count' that takes one parameter. 
+#   The parameter is a dictionary that maps from strings of names to the string "online" or "offline".
+
+#   Function should return the number of people who are online
+
